c42913c9fb688824f3d2d938c5faf5fb640d6fc0
John <johnarpaul@gmail.com>
2019-03-07 16:03:28 +0000

    Resolved changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
 
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
 end
