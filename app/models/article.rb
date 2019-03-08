5f3ad425815cb2c3dad411fd0b065b510cbb99c3
John <johnarpaul@gmail.com>
2019-03-08 16:55:01 +0000

    finished article model spec

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,5 @@
 class Article < ApplicationRecord
   belongs_to :user
+
+  validates :title, :text, :user_id, presence: true
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { is_expected.to validate_presence_of :title }
+  it { is_expected.to validate_presence_of :text }
+  it { is_expected.to validate_presence_of :user_id }
+
 end
