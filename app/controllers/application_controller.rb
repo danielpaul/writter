c50a62146b668e351e28b08fa15f8829032e2f9c
John <johnarpaul@gmail.com>
2019-03-08 12:40:59 +0000

    added flash message

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
   include Pundit
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
-  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
 
   protected
   def configure_permitted_parameters
