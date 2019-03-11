15326afa1be3ee959dbacb1a86ab7b8cb4cbf88c
John <johnarpaul@gmail.com>
2019-03-11 17:20:38 +0000

    optimized controller spec

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -1,24 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
-  describe "GET #index" do
-    it "responds successfully with an HTTP 200 status code" do
-      get :index
-      expect(response).to have_http_status(200)
-    end
 
-    it "renders the index template" do
-      get :index
-      expect(response).to render_template("index")
+  render_views
+
+  let(:user) { create(:user) }
+  let(:article) { create(:article, user: user) }
+  let(:article_attributes) { attributes_for(:article) }
+
+  def get_show
+    get :show, params: { id: article }
+  end
+
+  def get_new
+    get :new
+  end
+
+  def get_edit
+    get :edit, params: { id: article }
+  end
+
+  def post_create
+    post :create, params: { article: article_attributes }
+  end
+
+  def put_update
+    put :update, params: { id: article, :article => {:name => "New name" } }
+  end
+
+  def delete_destroy
+    delete :destroy, params: { id: article }
+  end
+
+  describe "GET #index" do
+    it "directs to index" do
+      expect(get :index).to render_template("index")
     end
   end
 
-  describe "GET#show" do
-    let(:article) { create(:article) }
-    
-    before(:each) do
-      get :show, params: { id: article }
-    end
+  describe "GET #show" do
+
+    before(:each) do get_show end
 
     it "assigns the requested article to @article" do
       expect(assigns(:article)).to eq(article)
@@ -29,73 +51,65 @@ RSpec.describe ArticlesController, type: :controller do
     end
   end
 
-  describe "GET#new" do
+  describe "GET #new" do
     context "logged out user" do
       it "redirects to sign in" do
-        get :new
-        expect(response).to redirect_to new_user_session_path
+        expect(get :new).to redirect_to new_user_session_path
       end
     end
 
     context "logged in user" do
-      it "creates a new article" do
-        user = create(:user)
+
+      before(:each) do
         sign_in user
-        get :new
+        get_new
+      end
+
+      it "creates a new article" do
         expect(assigns(:article)).to be_a_new(Article)
       end
 
       it "renders new with success" do
-        user = create(:user)
-        sign_in user
-        get :new
         expect(response).to render_template("new")
       end
     end
   end
 
-  describe "POST#create" do
+  describe "POST #create" do
     context "logged out user" do
       it "redirects to user sign in" do
-        post :create, params: { article: attributes_for(:article) }
-        expect(response).to redirect_to new_user_session_path
+        expect(post :create).to redirect_to new_user_session_path
       end
     end
 
     context "logged in user" do
+
+      before(:each) do
+        sign_in user
+      end
+
       context "with valid attributes" do
         it "creates a new article" do
-          user = create(:user)
-          sign_in user
-
-          expect{
-            post :create, params: { article: attributes_for(:article) }
+          expect {
+            post_create
           }.to change(Article, :count).by(1)
         end
       end
-      context "with invalid attrubutes" do
+
+      context "with invalid attributes" do
         it "does not create an article" do
-          user = create(:user)
-          sign_in user
-
-          #debugger
-
-          expect{
+          expect {
             post :create, params: { article: attributes_for(:article, title: nil, text: nil) }
           }.to change(Article, :count).by(0)
-
         end
       end
     end
   end
 
-  describe "GET#edit" do
+  describe "GET #edit" do
     context "logged out user" do
       it "redirects to user sign in" do
-        article = create(:article)
-        get :edit, params: { id: article }
-
-        expect(response).to redirect_to new_user_session_path
+        expect(get_edit).to redirect_to new_user_session_path
       end
     end
 
@@ -105,34 +119,30 @@ RSpec.describe ArticlesController, type: :controller do
           user = create(:user)
           sign_in user
 
-          article = create(:article)
-          get :edit, params: { id: article }
-
-            expect(response).to redirect_to root_path
+          expect(get_edit).to redirect_to root_path
         end
       end
+
       context "is author" do
-        it "allows edit" do
-          article = create(:article)
-          user = article.user
+        before(:each) do
           sign_in user
+          get_edit
+        end
 
-          get :edit, params: { id: article }
-
+        it "allows edit" do
           expect(assigns(:article)).to eq(article)
+        end
+        it "renders edit" do
           expect(response).to render_template("edit")
         end
       end
     end
   end
 
-  describe "POST#update" do
+  describe "PUT #update" do
     context "logged out user" do
       it "redirects to user sign in" do
-        article = create(:article)
-        post :update, params: { id: article }
-
-        expect(response).to redirect_to new_user_session_path
+        expect(put_update).to redirect_to new_user_session_path
       end
     end
 
@@ -142,55 +152,66 @@ RSpec.describe ArticlesController, type: :controller do
           user = create(:user)
           sign_in user
 
-          article = create(:article)
-          post :update, params: { id: article }
-
-          expect(response).to redirect_to root_path
+          expect(put_update).to redirect_to root_path
         end
       end
       context "is author" do
-        it "allows update" do
-          article = create(:article)
-          user = article.user
+        before(:each) do
           sign_in user
-
           post :update, params: { id: article, article: attributes_for(:article, title: "test") }
+        end
+
+        it "allows update" do
           expect(Article.find(article.id).title).to eq('test')
+        end
+        it "redirects to article path" do
           expect(response).to redirect_to article_path(article)
         end
       end
     end
   end
 
-  describe "DELETE#destroy" do
+  describe "DELETE #destroy" do
     context "logged out user" do
       it "redirects to user sign in" do
-        article = create(:article)
-        delete :destroy, params: { id: article }
-
-        expect(response).to redirect_to new_user_session_path
+        expect(delete_destroy).to redirect_to new_user_session_path
       end
     end
+
     context "logged in user" do
+
+      before(:each) do
+        article.reload
+      end
+
       context "not author" do
-        it "does not allow delete" do
+        before(:each) do
           user = create(:user)
           sign_in user
+        end
 
-          article = create(:article)
-          expect {delete :destroy, params: { id: article }}.to change{ Article.count }.by(0)
-          expect(response).to redirect_to root_path
+        it "does not allow delete" do
+          expect {
+            delete_destroy
+          }.to change(Article, :count).by(0)
+        end
+        it "redirects to root path" do
+          expect(delete_destroy).to redirect_to root_path
         end
       end
 
       context "is author" do
-        it "deletes the article" do
-          article = create(:article)
-          user = article.user
+        before(:each) do
           sign_in user
+        end
 
-          expect {delete :destroy, params: { id: article }}.to change{ Article.count }.by(-1)
-          expect(response).to redirect_to articles_path
+        it "deletes the article" do
+          expect {
+            delete_destroy
+          }.to change(Article, :count).by(-1)
+        end
+        it "redirects to articles path" do
+          expect(delete_destroy).to redirect_to articles_path
         end
       end
     end
