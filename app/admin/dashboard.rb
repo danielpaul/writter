10b19de67dc0213013fac475dee214403d91c66a
John <johnarpaul@gmail.com>
2019-03-13 11:22:54 +0000

    finished basic dashboard layout

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -3,12 +3,31 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc{ I18n.t("active_admin.dashboard") }
 
   content title: proc{ I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Recent Users" do
+          table_for User.order("id desc").limit(5).each do |_user|
+            column(:username)    { |user| link_to(user.username, admin_user_path(user)) }
+            column(:first_name)    { |user| link_to(user.first_name, admin_user_path(user)) }
+            column(:last_name)    { |user| link_to(user.last_name, admin_user_path(user)) }
+            column(:email)    { |user| link_to(user.email, admin_user_path(user)) }
+          end
+        end
       end
     end
+    columns do
+      column do
+        panel "Recent Articles" do
+          table_for Article.order("id desc").limit(5).each do |_article|
+            column(:title)    { |article| link_to(article.title, admin_article_path(article)) }
+            column(:content)    { |article| link_to(article.text, admin_article_path(article)) }
+            column(:author)    { |article| link_to(article.user_id, admin_user_path(article.user_id)) }
+          end
+        end
+      end
+    end
+  end
+end
 
     # Here is an example of a simple dashboard with columns and panels.
     #
@@ -29,5 +48,4 @@ ActiveAdmin.register_page "Dashboard" do
     #     end
     #   end
     # end
-  end # content
-end
+    # content
--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ApplicationRecord
 
 
   def send_first_article_email
-    if user.articles.count == 1 || true
+    if user.articles.count == 1
       ArticlesMailer.first_article(self.id).deliver_later
     end
   end
