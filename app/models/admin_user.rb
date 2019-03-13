f100727ed620c33ecee418dd830321c46700d5a0
John <johnarpaul@gmail.com>
2019-03-13 11:45:47 +0000

    fixed changes

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -1,6 +1,5 @@
 class AdminUser < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable,
+         :recoverable, :rememberable, :validatable,
+         :lockable, :timeoutable, :trackable
 end
