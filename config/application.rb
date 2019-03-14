e49e4b0c14174d3075848bc190ce1171cb9daaa2
John <johnarpaul@gmail.com>
2019-03-14 19:22:13 +0000

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -16,11 +16,6 @@ module Writter
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
-end
-
-module YourApp
-  class Application < Rails::Application
     config.middleware.use Rack::Deflater
   end
 end
