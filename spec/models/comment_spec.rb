3e07d52a0371744c57b9a2acbd39c2d07bc27037
John <johnarpaul@gmail.com>
2019-03-19 11:09:01 +0000

    Update comment_spec.rb

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -5,5 +5,9 @@ RSpec.describe Comment, :type => :model do
   it { is_expected.to have_db_column(:commentable_type).of_type(:string) }
   it { is_expected.to have_db_column(:body).of_type(:text) }
 
+  it { is_expected.to validate_presence_of :commentable_id }
+  it { is_expected.to validate_presence_of :commentable_type }
+  it { is_expected.to validate_presence_of :body }
+
   it { is_expected.to belong_to(:commentable) }
 end
