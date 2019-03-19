f2c1e3dea6fedb73042c90bdefa50dcab6ed9c67
John <johnarpaul@gmail.com>
2019-03-19 10:39:45 +0000

    Update 20190314201239_create_comments.rb

--- db/migrate/20190314201239_create_comments.rb
+++ db/migrate/20190314201239_create_comments.rb
@@ -1,10 +1,10 @@
 class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
-      t.string :commentable_type
-      t.integer :commentable_id
-      t.integer :user_id
-      t.text :body
+      t.string :commentable_type, allow_nil: false, allow_blank: false
+      t.integer :commentable_id, allow_nil: false, allow_blank: false
+      t.integer :user_id, allow_nil: false, allow_blank: false
+      t.text :body, allow_nil: false, allow_blank: false
 
       t.timestamps
     end
