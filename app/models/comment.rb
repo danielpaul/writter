8cf455b268bd9bcc5be8bee1cf1bbf2101c262f9
John <johnarpaul@gmail.com>
2019-03-19 11:08:19 +0000

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :commentable, polymorphic: true
-  validates :body, :user_id, presence: true
+  validates :body, :user_id, :commentable_type, :commentable_id,  presence: true
   validates_length_of :body, maximum: 1000, allow_blank: false
 end
