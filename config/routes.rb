291bfdd5ae22f497742b9bc653a412e816602e56
John <johnarpaul@gmail.com>
2019-03-20 15:28:46 +0000

    added notification views and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,18 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  mount Notifications::Engine => "/notifications"
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+  root to: 'articles#index'
+
   devise_for :users
+  resources :users, only: [:show]
 
   resources :articles do
     resources :comments, module: :articles, only: [:create, :destroy]
   end
 
-  root to: 'articles#index'
+  mount Notifications::Engine => "/notifications"
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
 end
