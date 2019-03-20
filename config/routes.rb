f1a117acee927cd06373db1f83616778799291e2
John <johnarpaul@gmail.com>
2019-03-20 14:07:14 +0000

    installed notification gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
+  mount Notifications::Engine => "/notifications"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   devise_for :users
