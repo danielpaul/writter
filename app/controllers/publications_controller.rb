cc8fcc226a8f63406c1f08ad834494b5734dac94
John <johnarpaul@gmail.com>
2019-03-27 15:28:03 +0000

    Update publications_controller.rb

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -2,34 +2,34 @@ class PublicationsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_publication, only: [:show, :edit, :update, :destroy]
   after_action :verify_authorized
+  after_action :add_to_publications_users, only: [:create]
 
   def index
     @publications = Publication.all
     authorize @publications
-    #set_meta_tags title: 'Articles'
+    set_meta_tags title: 'All Publications'
   end
 
   def show
-      authorize @publication
+    authorize @publication
   end
 
-  # GET /articles/new
+  # GET /publications/new
   def new
     @publication = current_user.publications.new
     authorize @publication
-    #set_meta_tags title: 'New Article'
+    set_meta_tags title: 'New Publication'
   end
 
-  # GET /articles/1/edit
+  # GET /publications/1/edit
   def edit
-      authorize @publication
+    authorize @publication
     #set_meta_tags title: "Edit #{@article.title}"
   end
 
-  # POST /articles
+  # POST /publications
   def create
     @publication = current_user.publications.new(publication_params)
-    @publication.users << current_user
     authorize @publication
 
     if @publication.save
@@ -40,12 +40,12 @@ class PublicationsController < ApplicationController
   end
 
   def update
-      authorize @publication
-      if @publication.update(publication_params)
-        redirect_to @publication, notice: 'Article was successfully updated.'
-      else
-        render :edit
-      end
+    authorize @publication
+    if @publication.update(publication_params)
+      redirect_to @publication, notice: 'Article was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
@@ -58,6 +58,10 @@ class PublicationsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_publication
       @publication = Publication.find(params[:id])
+    end
+
+    def add_to_publications_users
+      @publication.users << current_user
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
